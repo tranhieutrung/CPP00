67ee8acf1027c32858abfb2a23adbff9653cb3c7
Hieu Trung Tran <hitran@c1r2p5.hive.fi>
2025-01-09 10:26:29 +0200

    Clean the code of ex00.

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: hitran <hitran@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 12:54:40 by hitran            #+#    #+#             */
-/*   Updated: 2025/01/08 13:29:00 by hitran           ###   ########.fr       */
+/*   Updated: 2025/01/09 10:20:33 by hitran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,9 @@ int main(int ac, char **av) {
 	if (ac == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else {
-		for (int index = 1; index < ac; index++) {
-			for (int str_index = 0; av[index][str_index]; str_index++) {
-				std::cout << (char)(toupper(av[index][str_index]));
-			}
-		}
+		for (int i = 1; i < ac; i++)
+			for (int j = 0; av[i][j]; j++)
+				std::cout << (char)(toupper(av[i][j]));
 		std::cout << std::endl;
 	}
 	return 0;
