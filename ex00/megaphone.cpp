11b84f8e8e5288a2d7c7fd373bceddf2670f278b
Hieu Trung Tran <hitran@c1r2p5.hive.fi>
2025-01-10 15:07:30 +0200

    Updated the code using C++ manners.

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: hitran <hitran@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 12:54:40 by hitran            #+#    #+#             */
-/*   Updated: 2025/01/09 10:20:33 by hitran           ###   ########.fr       */
+/*   Updated: 2025/01/10 15:06:46 by hitran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,12 @@ int main(int ac, char **av) {
 	if (ac == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else {
-		for (int i = 1; i < ac; i++)
-			for (int j = 0; av[i][j]; j++)
-				std::cout << (char)(toupper(av[i][j]));
+		for (int i = 1; i < ac; i++) {
+			std::string arg = av[i];
+			
+			for (char c: arg) // range-based for loop
+				std::cout << (char)std::toupper(c);
+		}
 		std::cout << std::endl;
 	}
 	return 0;
