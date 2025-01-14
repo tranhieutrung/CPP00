178db946ff9e6da305b33f8e9d812d1b6b6b1e4f
tranhieutrung <tranhieutrung@outlook.com>
2025-01-14 23:13:51 +0200

    Completed ex00.

--- CPP02/ex00/Fixed.hpp
+++ CPP02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: hitran <hitran@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 22:38:11 by hitran            #+#    #+#             */
-/*   Updated: 2025/01/14 22:39:56 by hitran           ###   ########.fr       */
+/*   Updated: 2025/01/14 23:03:17 by hitran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,21 @@
 
 # include <iostream>
 
+// Fixed-point number: fixed<fixed-point number value, fractional bits>:
+// fixed-point number value: total bits
+// fractional bits: total bits for the fractional part
 class Fixed
 {
 	public:
-
+		Fixed();
+		Fixed(const Fixed &fixed);
+		~Fixed();
+		Fixed &operator = (const Fixed &fixed);
+		int getRawBits( void ) const;
+		void setRawBits( int const raw );
 	private:
+		int _value;
+		static const int _fBits = 8;
 };
+
+
--- CPP02/ex00/main.cpp
+++ CPP02/ex00/main.cpp
@@ -6,12 +6,20 @@
 /*   By: hitran <hitran@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 21:35:38 by hitran            #+#    #+#             */
-/*   Updated: 2025/01/14 22:39:34 by hitran           ###   ########.fr       */
+/*   Updated: 2025/01/14 23:06:26 by hitran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Replace.hpp"
+#include "Fixed.hpp"
+#include <iostream>
 
-int main() {
-
+int main( void ) {
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return 0;
 }
