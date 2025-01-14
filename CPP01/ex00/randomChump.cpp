d12268b237772ca738f560f89c9b13d612638a03
Hieu Trung Tran <hitran@c1r2p5.hive.fi>
2025-01-14 14:35:10 +0200

    Edited the randomChump.cpp to create zombie on the stack instead of the heap.

--- CPP01/ex00/randomChump.cpp
+++ CPP01/ex00/randomChump.cpp
@@ -6,20 +6,13 @@
 /*   By: hitran <hitran@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 14:58:05 by hitran            #+#    #+#             */
-/*   Updated: 2025/01/13 13:02:30 by hitran           ###   ########.fr       */
+/*   Updated: 2025/01/14 14:30:46 by hitran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 void randomChump(std::string name) {
-	try {
-		Zombie *zombie = new Zombie(name);
-	
-		zombie->announce();
-		delete (zombie);
-	} catch (const std::bad_alloc &error) {
-		std::cerr << "Memory allocation failed: " << error.what() << std::endl;
-		exit (1);
-	}
+		Zombie zombie(name);
+		zombie.announce();
 }
