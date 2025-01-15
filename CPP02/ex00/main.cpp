ffc3ee8c8d15aa5d5e3c2934b0d4af4c9c9a7df9
Hieu Trung Tran <hitran@c1r3p9.hive.fi>
2025-01-15 13:21:57 +0200

    Cleant the ex00's code.

--- CPP02/ex00/main.cpp
+++ CPP02/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hitran <hitran@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 21:35:38 by hitran            #+#    #+#             */
-/*   Updated: 2025/01/14 23:06:26 by hitran           ###   ########.fr       */
+/*   Updated: 2025/01/15 13:21:13 by hitran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@ int main( void ) {
 	Fixed a;
 	Fixed b( a );
 	Fixed c;
+
 	c = b;
+
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
